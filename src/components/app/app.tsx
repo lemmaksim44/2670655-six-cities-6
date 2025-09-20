5a67ef349508b8868da18399e3458550ed544d5f
Maxim Lezhankov <lemmaksim44@gmail.com>
2025-09-20 10:11:45 +0300

    feat: Added main page, card component and app file

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainPage from "../../page/main-page/main-page";
+
+type AppProps = {
+  cardQuantity: number;
+}
+
+function App({cardQuantity}: AppProps): JSX.Element {
+  return (
+    <MainPage cardQuantity={cardQuantity}/>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+
+const Setting = {
+  CardQuantity: 5,
+} as const;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +12,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App cardQuantity={Setting.CardQuantity}/>
   </React.StrictMode>
 );
