e25ecfaad188318634eddd760e64618eb4a11ad8
Maxim Lezhankov <lemmaksim44@gmail.com>
2025-09-29 13:28:22 +0300

    feat: Added 404 page component

---
+++ src/page/page-404/page-404.tsx
@@ -0,0 +1,50 @@
+function Page404() {
+  return (
+    <div className="page page--favorites-empty">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    <span className="header__favorite-count">0</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="page-404">
+        <div className="page-404__block container">
+            <img src="img/404_2.gif" className="gif-404"/>
+            <p className="text-404">404 - Page not found</p>
+            <a className="button-404" href="/">Back to Main Screen</a>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default Page404
