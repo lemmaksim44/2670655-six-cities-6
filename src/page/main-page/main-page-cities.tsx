3e24081c62e9699a6cccc777161aa82767f71820
Maxim Lezhankov <lemmaksim44@gmail.com>
2025-11-12 14:55:25 +0300

    feat: Added the ability to sort cards

--- src/page/main-page/main-page-cities.tsx
+++ src/page/main-page/main-page-cities.tsx
@@ -1,10 +1,13 @@
 import Card from '../../components/card/card';
 import { OfferPreviewType } from '../../types/offer-preview';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import Map from '../../components/map/map';
 import { plural } from '../../utils/scripts';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import MainPageCitiesFilter from './main-page-cities-filter';
+import { SortingOptions } from '../../const';
+import { sortOffers } from '../../utils/sorts';
 
 type MainPageCitiesProps = {
   offers: OfferPreviewType[];
@@ -18,35 +21,27 @@ function MainPageCities({offers}: MainPageCitiesProps) {
     setHoveredCardId(id);
   }
 
+  const [currentSortOption, setCurrentSortOption] = useState(SortingOptions.Popular);
+  const sortedOffers = useMemo(
+    () => sortOffers(offers, currentSortOption),
+    [offers, currentSortOption]
+  );
+
   return (
     <div className="cities">
       <div className="cities__places-container container">
         <section className="cities__places places">
           <h2 className="visually-hidden">Places</h2>
           <b className="places__found">{offers.length} {plural(offers.length, 'place')} to stay in {city}</b>
-          <form className="places__sorting" action="#" method="get">
-            <span className="places__sorting-caption">Sort by</span>
-            <span className="places__sorting-type" tabIndex={0}>
-              Popular
-              <svg className="places__sorting-arrow" width="7" height="4">
-                <use xlinkHref="#icon-arrow-select"></use>
-              </svg>
-            </span>
-            <ul className="places__options places__options--custom places__options--opened">
-              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-              <li className="places__option" tabIndex={0}>Price: low to high</li>
-              <li className="places__option" tabIndex={0}>Price: high to low</li>
-              <li className="places__option" tabIndex={0}>Top rated first</li>
-            </ul>
-          </form>
+          <MainPageCitiesFilter currentSortOption={currentSortOption} onChangeSort={setCurrentSortOption}/>
           <div className="cities__places-list places__list tabs__content">
-            {offers.map((offer) => (
+            {sortedOffers.map((offer) => (
               <Card key={offer.id} offer={offer} block='cities' onMouseHover={handleCardHover} />
             ))}
           </div>
         </section>
         <div className="cities__right-section">
-          <Map offers={offers} block='cities__map' selectedOfferId={hoveredCardId}/>
+          <Map offers={sortedOffers} block='cities__map' selectedOfferId={hoveredCardId}/>
         </div>
       </div>
     </div>
