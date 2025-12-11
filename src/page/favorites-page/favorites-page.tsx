6392015bb71abb6d9878102461ad5ca73223960e
Maxim Lezhankov <lemmaksim44@gmail.com>
2025-12-11 12:26:31 +0300

    feat: Added loading spinner to favorites page

--- src/page/favorites-page/favorites-page.tsx
+++ src/page/favorites-page/favorites-page.tsx
@@ -3,21 +3,27 @@ import Footer from '../../components/footer/footer';
 import FavoritesPageEmpty from './favorites-page-empty';
 import FavoritesPageList from './favorites-page-list';
 import { useSelector } from 'react-redux';
-import { selectIsFavorites, selectGroupedFavorites } from '../../store/favorite/selector';
+import { selectIsFavorites, selectGroupedFavorites, selectFavoritesLoadingStatus } from '../../store/favorite/selector';
+import Spinner from '../../components/spinner/spinner';
 
 function FavoritesPage() {
   const hasOffers = useSelector(selectIsFavorites)
   const groupCitiesOffer = useSelector(selectGroupedFavorites);
+  const isLoading = useSelector(selectFavoritesLoadingStatus);
 
   return (
     <div className="page">
       <Header/>
 
-      <main className={`page__main page__main--favorites ${hasOffers ? 'page__main--favorites-empty' : ''}`}>
-        <div className="page__favorites-container container">
-          {hasOffers ? <FavoritesPageList offers={groupCitiesOffer}/> : <FavoritesPageEmpty/>}
-        </div>
-      </main>
+      {isLoading ? (
+        <Spinner/>
+      ) : (
+        <main className={`page__main page__main--favorites ${hasOffers ? 'page__main--favorites-empty' : ''}`}>
+          <div className="page__favorites-container container">
+            {hasOffers ? <FavoritesPageList offers={groupCitiesOffer}/> : <FavoritesPageEmpty/>}
+          </div>
+        </main>
+      )}
 
       <Footer/>
     </div>
